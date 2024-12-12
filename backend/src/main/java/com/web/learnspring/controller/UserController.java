49f3a8aad16882b3fb44ff5d3496eb2a0ad3f670
ttoannguyen <codehub.personal@gmail.com>
2024-12-12 16:12:01 +0700

    update

---
+++ backend/src/main/java/com/web/learnspring/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.web.learnspring.controller;
+
+import com.web.learnspring.model.User;
+import com.web.learnspring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
